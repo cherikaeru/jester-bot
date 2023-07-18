75c88d5d090dada098ead3e838b16b42f08fab2b
Luke Walker <lukeww101@gmail.com>
2023-07-18 21:21:23 +0100

    Fixed bugs

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
-const token = String(process.env.TOKEN);
-const serverid = String(process.env.SERVERID);
-const roleid = String(process.env.ROLEID);
-const channelid = String(process.env.CHANNELID);
+const token = process.env.TOKEN;
+const serverid = process.env.SERVERID;
+const roleid = process.env.ROLEID;
+const channelid = process.env.CHANNELID;
 
 const fs = require('fs');
 const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js')
@@ -26,7 +26,9 @@ let userList = {
 if (fs.existsSync('userlist.json')) {
   const userListData = fs.readFileSync('userlist.json');
   userList = JSON.parse(userListData);
-  userList.users = Array.from(userList.users);
+  if (!userList.users) {
+    userList.users = Array.from(userList.users);
+  }
 }
 else {
   const userListData = JSON.stringify(userList, null, 2);
@@ -76,31 +78,35 @@ function assignRoleToUser() {
     return;
   }
 
-  // Remove role from previous user
-  if (userList.assignedUser !== null) {
-    const member = guild.members.cache.get(userList.assignedUser);
-    member.roles.remove(role);
-    console.log(`Removed role from user ${member.user.username}`);
-  }
-
   // Assign role to new random user
-  const random = Math.floor(Math.random() * userList.users.length);
+  var random = Math.floor(Math.random() * userList.users.length);
   while (userList.users[random] == userList.assignedUser && userList.users.length > 1) {
     random = Math.floor(Math.random() * userList.users.length);
   }
 
-  const member = guild.members.cache.get(userList.users[random]);
+  // Remove role from previous user
+  if (userList.assignedUser != null) {
+    console.log(userList.assignedUser)
+    let old = guild.members.cache.get(userList.assignedUser);
+    guild.members.cache.get(userList.assignedUser).roles.remove(role).then(() => {
+      console.log(`Removed role from user ${old.user.username}`);
+    }).catch((error) => {
+      console.error(`Failed to remove role from user ${old.user.username}: ${error}`);
+    });
+  }
+
+  var member = guild.members.cache.get(userList.users[random]);
 
   if (member) {
     member.roles.add(role).then(() => {
       console.log(`Assigned role to user ${member.user.username}`);
       const embed = new EmbedBuilder()
         .setColor(0xe0707c)
-        .setTitle('A New Jester is Being Declared!')
+        .setTitle(`A New ${role.name} is Being Declared!`)
         .setAuthor({ name: `${client.user.username}, the towncrier`, iconURL: `https://cdn.discordapp.com/avatars/${client.user.id}/${client.user.avatar}.webp`, url: 'https://cherikaeru.github.io/' })
         .setDescription('Hear ye, hear ye...')
         .setThumbnail(`https://cdn.discordapp.com/avatars/${member.user.id}/${member.user.avatar}.webp`)
-        .addFields({ name: `**The new jester is ${member.user.username}**`, value: '**Point and laugh at them NOW**' })
+        .addFields({ name: `**The new ${role.name} is ${member.user.username}**`, value: '👏' })
         .setTimestamp()
         .setFooter({ text: 'Bot made by cherry (@cherikaeru)', iconURL: `https://images-ext-1.discordapp.net/external/OsFlFECjRgZhjWxHXdsklJJiue5b_FUshIvJsx1BYYI/https/cdn.discordapp.com/avatars/271370042627588096/c08dabc6468147b37e75c2a63fd34798.webp`, url: 'https://cherikaeru.github.io/' });
       channel.send({ embeds: [embed] });
@@ -135,7 +141,7 @@ function getNextDay() {
   const dayOfWeek = now.getDay();
   const daysUntilNextDay = dayOfWeek <= 2 ? 2 - dayOfWeek : 9 - dayOfWeek;
   const nextDay = new Date(now.getFullYear(), now.getMonth(), now.getDate() + daysUntilNextDay);
-  nextDay.setHours(18, 30, 0, 0); // Set time to 6:30 PM
+  nextDay.setHours(21, 4, 0, 0); // Set time to 6:30 PM
   return nextDay;
 }
 
